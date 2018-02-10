546ab57614783c28b267a32b9a51695eae93c521
danielroa98 <a01021960@itesm.mx>
2018-02-10 00:17:06 -0600

    Verison (casi) final

--- Tarea_Integradora.java
+++ Tarea_Integradora.java
@@ -1,60 +1,59 @@
 import java.util.*;
 
-public class Tarea_Integradora
-{
-  public static void main(String[] args)
-  {
-    Scanner lectura = new Scanner(System.in);
+public class Tarea_Integradora {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
 
-    String a = "A", b = "B", c = "C";
-    String opcion, codigo;
-    String cod = "cod", dec = "dec";
-    int deco, caracter, cp;
-    char car;
+        String opcion, codigo;
+        String cod = "cod", dec = "dec";
+        int deco, caracter, cp, cant;
+        int secreto;
+        char car;
+        //int secreto [];
 
-    System.out.println("Que va a hacer? Codificar o Decodificar?");
-    System.out.println("*");
-    System.out.println("Por favor, solo escriba "+cod+" para Codificar o "+dec+" para Decodificar");
-    System.out.println("*");
-    System.out.print("Inserte aqui el comando: ");
-    opcion = lectura.nextLine();
+        System.out.println("Que va a hacer? Codificar o Decodificar?");
+        System.out.println("*");
+        System.out.println("Por favor, solo escriba " + cod + " para Codificar o " + dec + " para Decodificar");
+        System.out.println("*");
+        System.out.print("Inserte aqui el comando: ");
+        opcion = lectura.nextLine();
 
-//if usado para codificar la oración
-    if(opcion.equalsIgnoreCase("COD"))
-    {
-      System.out.println("Inserte el texto que se va a codificar");
-      codigo = lectura.nextLine();
+        //if usado para codificar la oración
+        if (opcion.equalsIgnoreCase("COD")) {
+            System.out.println("Inserte el texto que se va a codificar");
+            codigo = lectura.nextLine();
 
-      System.out.println("");
-      for (int i=0; i<codigo.length(); i++)
-      {
-        caracter = codigo.charAt(i);
-        //System.out.printf("%c ", caracter);
+            System.out.println("");
+            for (int i = 0; i < codigo.length(); i++) {
+                caracter = codigo.charAt(i);
+                //System.out.printf("%c ", caracter);
 
-        cp = codigo.codePointAt(i);
-        System.out.printf(" %d ", cp);
+                cp = codigo.codePointAt(i);
+                secreto = cp * 3;
 
-      }
+                System.out.printf(" %d ", secreto);
 
+            }
+
+        }
+
+        //else usado para decodificar la oración
+        else if (opcion.equalsIgnoreCase("DEC")) {
+            System.out.println("Con cuantos caracteres cuenta su código?");
+            cant = lectura.nextInt();
+
+            for (int cont = 0; cont < cant; cont++) {
+                System.out.println("Inserte su valor " + cont + ": ");
+                deco = lectura.nextInt();
+
+                secreto = deco / 3;
+
+                car = (char)secreto;
+
+                System.out.println("");
+                System.out.println(" " + car);
+
+            }
+        }
     }
-
-//else usado para decodificar la oración
-    else if(opcion.equalsIgnoreCase("DEC"))
-    {
-      System.out.println("Inserte el texto que se va a decodificar");
-      deco = lectura.nextInt();
-
-    /*  while ()
-      {
-
-      }
-      */
-      car = (char)deco;
-
-      System.out.println("");
-      System.out.println(" "+car);
-
-    }
-
-  }
 }
